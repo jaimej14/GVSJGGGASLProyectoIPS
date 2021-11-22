a56d4c3ca908071aa1892d5a563a0698138f9eb7
jaime <jaime@LAPTOP-BMCADMKL>
2021-11-22 14:18:22 +0100

    Add project files.

---
+++ Dsl/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.ConstrainedExecution;
+
+#endregion
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle(@"")]
+[assembly: AssemblyDescription(@"")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(@"UPM_IPS")]
+[assembly: AssemblyProduct(@"GVSJGGGASLProyectoIPS")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: System.Resources.NeutralResourcesLanguage("en")]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion(@"1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: ReliabilityContract(Consistency.MayCorruptProcess, Cer.None)]
+
+//
+// Make the Dsl project internally visible to the DslPackage assembly
+//
+[assembly: InternalsVisibleTo(@"UPM_IPS.GVSJGGGASLProyectoIPS.DslPackage, PublicKey=0024000004800000940000000602000000240000525341310004000001000100FDA9566FC4700E79BFF128A4802CCED1E478E3DB057EEEEC98CAB9A9B7EF31641FA1772C1E82EFFA97E6FC60DDD6D509EA8263ACF21EBC96CA97ABA544B5EA541091F5B8B5E7949AAD6604760C3B10CE823E6748A658CD2ACFD79D06775D61E02B7E21286E2C1920F681CB4774A1A2C11F5DC712E5BF5D30162481BF8E19CFB4")]
